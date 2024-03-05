f551fdf4d752a881809329ae5ea85dd507f13271
SHU <chulie2003@gmail.com>
2024-03-05 15:30:16 -0800

    Added Utilities class file to mvc package

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+//git add git bra.  or git (pathing/file)
+//git commit (saves the changes) -m " "
+
+//git pull
+//git push
+
+//git branch/fetch/
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
