b48be9ea41b8b4ec284794974d8c460213571c9f
Kenmin <kenminho@gmail.com>
2024-03-14 15:32:37 -0700

    add buttons

--- src/gameOfLife/LifePanel.java
+++ src/gameOfLife/LifePanel.java
@@ -7,10 +7,24 @@ import mvc.AppPanel;
 import stopLight.StoplightFactory;
 import stopLight.StoplightPanel;
 
+import javax.swing.*;
+
 public class LifePanel extends GridPanel {
+
+    private JButton change;
     public LifePanel(GridFactory factory) {
+
         super(factory);
+        change = new JButton("RUN1");
+        change.addActionListener(this);
+        ControlPanel.add(change);
+        change = new JButton("RUN50");
+        change.addActionListener(this);
+        ControlPanel.add(change);
+        this.display();
     }
+
+
     public static void main(String[] args) {
         GridFactory factory = new LifeFactory();
         GridPanel panel = new LifePanel((LifeFactory)factory);
